f5af673b6f65df20f76bda8f2ddd40b2aba89d8b
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-10 12:20:02 -0400

    copy paste

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,12 +1,22 @@
 import { Component } from "react";
 
-const count = 0;
-
 class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    // Use this.setState() to add 1 to the count
+  };
+
   render() {
     return (
       <>
-        <h1>The count is: {count}</h1>
+        <h1>The count is: {this.state.count}</h1>
+        {/* pass a prop called onClick to the button */}
         <button>+1</button>
       </>
     );
